cc9bf9e8fecaf65b057fdd3d24093f0fdf4e5909
David <fanefaraon696@gmail.com>
2023-04-04 17:31:30 +0300

    ADDED: handle on close and handle on open methods in are you sure modal

--- frontend/rent-a-car/src/components/AreYouSureModal/AreYouSureModal.tsx
+++ frontend/rent-a-car/src/components/AreYouSureModal/AreYouSureModal.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { FC, useState } from "react";
+import styles from "./AreYouSureModal.module.css";
+import { Box, Modal, Typography } from "@mui/material";
 
-export const AreYouSureModal = () => {
-  return <div>YesOrNoModal</div>;
+interface IAreYouSureModal {}
+
+export const AreYouSureModal: FC<IAreYouSureModal> = ({}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOnClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleOnOpen = () => {
+    setIsOpen(true);
+  };
+
+  return (
+    <Modal open={isOpen} onClose={handleOnClose}>
+      <Box>
+        <Typography>Are you sure you want to continue?</Typography>
+      </Box>
+    </Modal>
+  );
 };

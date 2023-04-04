3ca505fb46314bccbfdfdc2d3bc33380d3634b1a
David <fanefaraon696@gmail.com>
2023-04-04 21:33:31 +0300

    added more styling to are you sure modal

--- frontend/rent-a-car/src/components/AreYouSureModal/AreYouSureModal.tsx
+++ frontend/rent-a-car/src/components/AreYouSureModal/AreYouSureModal.tsx
@@ -6,7 +6,7 @@ interface IAreYouSureModalProps {
   onOkClick: () => void;
   onCancelClick: () => void;
   onClose?: () => void;
-  text?: () => void;
+  text?: string;
   isOpen: boolean;
 }
 
@@ -28,21 +28,29 @@ export const AreYouSureModal: FC<IAreYouSureModalProps> = ({
   return (
     <Modal open={isOpen} onClose={handleOnClose}>
       <Box sx={style}>
-        <Typography>Are you sure you want to continue?</Typography>
-        <Button
-          onClick={() => {
-            onOkClick();
-          }}
-        >
-          Yes
-        </Button>
-        <Button
-          onClick={() => {
-            onCancelClick();
-          }}
-        >
-          Cancel
-        </Button>
+        <Typography sx={{ textAlign: "center" }}>
+          {text ?? "Are you sure you want to continue?"}
+        </Typography>
+        <div className={styles.buttons}>
+          <Button
+            variant="contained"
+            sx={successButtonStyle}
+            onClick={() => {
+              onOkClick();
+            }}
+          >
+            Yes
+          </Button>
+          <Button
+            variant="contained"
+            sx={cancelButtonStyle}
+            onClick={() => {
+              onCancelClick();
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
       </Box>
     </Modal>
   );
@@ -50,12 +58,30 @@ export const AreYouSureModal: FC<IAreYouSureModalProps> = ({
 
 const style = {
   position: "absolute",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
   top: "50%",
   left: "50%",
+  minHeight: "35%",
   transform: "translate(-50%, -50%)",
   width: 400,
   backgroundImage: "linear-gradient(to bottom right, #0097b9, #8769ae)",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+};
+
+const buttonStyle = {
+  width: "120px",
+};
+
+const successButtonStyle = {
+  ...buttonStyle,
+  backgroundColor: "limegreen",
+};
+
+const cancelButtonStyle = {
+  ...buttonStyle,
+  backgroundColor: "red",
 };

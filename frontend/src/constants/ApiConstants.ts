28ff635ba25d0501bcbefd2a5e203b7495c0c7e0
David <fanefaraon696@gmail.com>
2023-04-12 01:59:07 +0300

    switch back to http

--- frontend/src/constants/ApiConstants.ts
+++ frontend/src/constants/ApiConstants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "https://34.77.100.165/";
+export const baseUrl = "http://34.77.100.165/";
 
 export const clientController = "api/client/";
 export const vehicleController = "api/vehicle/";

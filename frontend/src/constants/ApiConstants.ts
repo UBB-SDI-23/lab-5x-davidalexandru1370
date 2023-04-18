f6f4bc69cc69643fd5f6928086847fce39a5bd40
David <fanefaraon696@gmail.com>
2023-04-18 19:35:46 +0300

    changed backend api route by enviroment variable

--- frontend/src/constants/ApiConstants.ts
+++ frontend/src/constants/ApiConstants.ts
@@ -1,4 +1,10 @@
-export const baseUrl = "/api/";
+const PRODUCTION_RentAVehicleApi_URL = "https://104.155.121.9/";
+const DEVELOPMENT_RentAVehicleApi_URL = "http://localhost:5199/";
+
+export const baseUrl =
+  (process.env.NODE_ENV === "development"
+    ? DEVELOPMENT_RentAVehicleApi_URL
+    : PRODUCTION_RentAVehicleApi_URL) + "/api/";
 
 export const clientController = "client/";
 export const vehicleController = "vehicle/";

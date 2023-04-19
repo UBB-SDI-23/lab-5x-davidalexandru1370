736b77af365df6d400ac2d9e1a24102841c43772
David <fanefaraon696@gmail.com>
2023-04-19 21:43:25 +0300

    ADDED: vehicle rents page and get rents paginated

--- frontend/src/pages/rents/index.tsx
+++ frontend/src/pages/rents/index.tsx
@@ -1,33 +1,76 @@
+import VehicleRent from "@/model/VehicleRent";
 import {
   Box,
+  CircularProgress,
   Paper,
   Table,
+  TableBody,
   TableCell,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getRentsPaginated } from "../api/RentsApi";
 
 export default function Rents() {
+  const [rents, setRents] = useState<VehicleRent[]>();
+  const [skip, setSkip] = useState<number>(0);
+  const take = 5;
+
+  useEffect(() => {
+    getRentsPaginated(skip, take).then((r) => {
+      setRents(r);
+    });
+  }, []);
+
   return (
     <div>
-      <Box
-        component={Paper}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          paddingTop: "2rem",
-        }}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell> </TableCell>
-            </TableRow>
-          </TableHead>
-        </Table>
-      </Box>
+      {rents === undefined ? (
+        <>
+          <CircularProgress />
+          <Typography>Loading data...</Typography>
+        </>
+      ) : (
+        <Box
+          component={Paper}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingTop: "2rem",
+          }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Car Plate</TableCell>
+                <TableCell>Client CNP</TableCell>
+                <TableCell>Client Name</TableCell>
+                <TableCell>Start Date</TableCell>
+                <TableCell>End Date</TableCell>
+                <TableCell>Total Cost</TableCell>
+                <TableCell>Comments</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rents.map((rent) => {
+                return (
+                  <TableRow key={rent.id}>
+                    <TableCell>{rent.vehicle.carPlate}</TableCell>
+                    <TableCell>{rent.client.cnp}</TableCell>
+                    <TableCell>{rent.client.name}</TableCell>
+                    <TableCell>{rent.startDate.toString()}</TableCell>
+                    <TableCell>{rent.endDate.toString()}</TableCell>
+                    <TableCell>{rent.totalCost}</TableCell>
+                    <TableCell>{rent.comments}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+      )}
     </div>
   );
 }

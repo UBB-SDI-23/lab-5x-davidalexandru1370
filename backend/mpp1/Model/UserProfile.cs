1658c40d7349fc3574245323701cabab54b1866d
davidalexandru1370 <fanefaraon696@gmail.com>
2023-05-01 00:47:40 +0300

    created user migrations

--- backend/mpp1/Model/UserProfile.cs
+++ backend/mpp1/Model/UserProfile.cs
@@ -1,23 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 using mpp1.Enums;
-    
+
 namespace mpp1.Model;
 
+[Table("UserProfile")]
 public class UserProfile
 {
-    [JsonIgnore]
-    public User User { get; set; }
-    [Key]
-    public User UserId { get; set; }
-    [Required]
-    public string Bio { get; set; } = null!;
-    [Required]
-    public string Location { get; set; } = null!;
-    [Required]
-    public DateOnly Birthday { get; set; }
-    [Required]
-    public GendersEnum Gender { get; set; }
-    [Required]
-    public MaritalStatusEnum MaritalStatus { get; set; }
+    [JsonIgnore] public User User { get; set; } = null!;
+    [Key] [ForeignKey("User")] public Guid UserId { get; set; } 
+    [Required] public string Bio { get; set; } = null!;
+    [Required] public string Location { get; set; } = null!;
+    [Required] public DateOnly Birthday { get; set; }
+    [Required] public GendersEnum Gender { get; set; }
+    [Required] public MaritalStatusEnum MaritalStatus { get; set; }
 }

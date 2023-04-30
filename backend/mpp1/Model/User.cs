726986f1258fc4820b6e402e07dbf79288438b1f
davidalexandru1370 <fanefaraon696@gmail.com>
2023-04-30 23:32:26 +0300

    created user model in backend

---
+++ backend/mpp1/Model/User.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mpp1.Model;
+
+[Table("User")]
+public class User
+{
+    [Key]
+    public Guid Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Password { get; set; }
+}

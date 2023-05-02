b83ac89822a36c11bef172dd26f72fb88bd159aa
davidalexandru1370 <fanefaraon696@gmail.com>
2023-05-02 23:22:59 +0300

    created new migration with token validation user table

---
+++ backend/mpp1/Migrations/20230502202057_CreatedTokenValidationUserTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mpp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedTokenValidationUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TokenValidationUser",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IssuedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TokenValidationUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TokenValidationUser_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenValidationUser_UserId",
+                table: "TokenValidationUser",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TokenValidationUser");
+        }
+    }
+}

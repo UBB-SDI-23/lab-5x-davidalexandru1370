4a7bc224d1b842f7deded1a88112be84ad4eb677
davidalexandru1370 <fanefaraon696@gmail.com>
2023-05-20 21:52:51 +0300

    scroll down

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,4 +1,10 @@
-import React, { useContext, useLayoutEffect, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import styles from "./chat.module.css";
 import { ChatContext } from "@/context/ChatContext/ChatContext";
 import { Box, List, Paper, TextField, Typography } from "@mui/material";
@@ -6,13 +12,49 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ClearIcon from "@mui/icons-material/Clear";
 import SendIcon from "@mui/icons-material/Send";
 import { AuthentificationContext } from "@/context/AuthentificationContext/AuthentificationContext";
+import ForumIcon from "@mui/icons-material/Forum";
+import { useId } from "react";
 
 export const Chat = () => {
   const { messages, sendMessage } = useContext(ChatContext);
   const [text, setText] = useState<string>("");
   const { userDto } = useContext(AuthentificationContext);
-  const messageListRef = useRef<HTMLDivElement>(null);
+  const [isChatVisible, setIsChatVisible] = useState<boolean>(false);
+  const [isScrollDown, setIsScrollDown] = useState<boolean>(false);
 
+  const messageListRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (node === null) {
+        return;
+      }
+      const setScrollDown = () => {
+        node.scrollTop = node.scrollHeight;
+      };
+
+      const handleScroll = () => {
+        if (node !== null) {
+          const scrollToBottomThreshold: number = 100;
+          const shouldScrollToBottom: boolean =
+            node.scrollHeight - node.scrollTop - node.clientHeight <=
+            scrollToBottomThreshold;
+          if (shouldScrollToBottom) {
+            node.scrollTop = node.scrollHeight;
+          }
+        }
+      };
+
+      if (isScrollDown === false) {
+        setScrollDown();
+        setIsScrollDown(true);
+      }
+
+      handleScroll();
+
+      return node;
+    },
+    [messages, isChatVisible]
+  );
+  const listId = useId();
   const handleSendMessage = async () => {
     if (text.trim().localeCompare("") !== 0) {
       await sendMessage({
@@ -23,96 +65,99 @@ export const Chat = () => {
     }
   };
 
-  useLayoutEffect(() => {
-    const handleScroll = () => {
-      if (messageListRef.current !== null) {
-        const list = messageListRef.current;
-        const scrollToBottomThreshold: number = 100;
-        const shouldScrollToBottom: boolean =
-          list.scrollHeight - list.scrollTop - list.clientHeight <=
-          scrollToBottomThreshold;
-
-        if (shouldScrollToBottom) {
-          messageListRef.current.scrollTop =
-            messageListRef.current.scrollHeight;
-        }
-      }
-    };
-
-    handleScroll();
-  }, [messages]);
-
   return (
-    <div className={styles.content}>
-      <div className={styles.header}>
-        <ClearIcon sx={{ cursor: "pointer" }} />
-      </div>
-      <Paper
-        ref={messageListRef}
-        style={{
-          backgroundColor: "white",
-          height: "100%",
-          overflow: "auto",
-        }}
-      >
-        <List sx={listStyle}>
-          {messages.map((message) => {
-            return (
-              <Box
-                sx={{
-                  display: "flex",
-                  borderTop: "1px solid white",
-                  paddingBlock: "4px",
-                }}
-              >
-                <AccountCircleIcon />
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    paddingLeft: "4px",
-                  }}
-                >
-                  <Typography sx={{ fontSize: "small" }}>
-                    {message.username}
-                  </Typography>
-                  <Typography>{message.text}</Typography>
-                </Box>
-              </Box>
-            );
-          })}
-        </List>
-      </Paper>
-      <div className={styles.sendMessageContainer}>
-        <TextField
-          multiline
-          value={text}
-          placeholder="Write your thoughts"
-          sx={{ width: "100%" }}
-          onKeyDown={async (event) => {
-            if (event.key === "Enter") {
-              event.preventDefault();
-              await handleSendMessage();
-            }
-          }}
-          onChange={(value) => {
-            setText(value.currentTarget.value);
-          }}
-          InputProps={{
-            endAdornment: (
-              <SendIcon
-                onClick={async () => {
+    <div>
+      {isChatVisible === false && (
+        <div className={styles.chatIconContainer}>
+          <ForumIcon
+            className={styles.chatIcon}
+            sx={{ cursor: "pointer" }}
+            onClick={() => {
+              setIsScrollDown(false);
+              setIsChatVisible(true);
+            }}
+          />
+        </div>
+      )}
+      {isChatVisible && (
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <ClearIcon
+              sx={{ cursor: "pointer" }}
+              onClick={() => {
+                setIsChatVisible(false);
+              }}
+            />
+          </div>
+          <Paper
+            ref={messageListRef}
+            key={listId}
+            style={{
+              backgroundColor: "white",
+              height: "100%",
+              overflow: "auto",
+            }}
+          >
+            <List sx={listStyle}>
+              {messages.map((message) => {
+                return (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      borderTop: "1px solid white",
+                      paddingBlock: "4px",
+                    }}
+                  >
+                    <AccountCircleIcon />
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        paddingLeft: "4px",
+                      }}
+                    >
+                      <Typography sx={{ fontSize: "small" }}>
+                        {message.username}
+                      </Typography>
+                      <Typography>{message.text}</Typography>
+                    </Box>
+                  </Box>
+                );
+              })}
+            </List>
+          </Paper>
+          <div className={styles.sendMessageContainer}>
+            <TextField
+              multiline
+              value={text}
+              placeholder="Write your thoughts"
+              sx={{ width: "100%" }}
+              onKeyDown={async (event) => {
+                if (event.key === "Enter") {
+                  event.preventDefault();
                   await handleSendMessage();
-                }}
-                sx={{
-                  color: `${text === "" ? "" : "blue"}`,
-                  cursor: `${text === "" ? "default" : "pointer"}`,
-                }}
-              />
-            ),
-          }}
-        ></TextField>
-      </div>
+                }
+              }}
+              onChange={(value) => {
+                setText(value.currentTarget.value);
+              }}
+              InputProps={{
+                endAdornment: (
+                  <SendIcon
+                    onClick={async () => {
+                      await handleSendMessage();
+                    }}
+                    sx={{
+                      color: `${text === "" ? "" : "blue"}`,
+                      cursor: `${text === "" ? "default" : "pointer"}`,
+                    }}
+                  />
+                ),
+              }}
+            ></TextField>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

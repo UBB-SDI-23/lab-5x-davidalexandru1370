ae778a6a6919762c355e56e43cc668776adb809d
davidalexandru1370 <fanefaraon696@gmail.com>
2023-05-20 23:20:38 +0300

    made user to choose a nickname

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,26 +1,40 @@
-import React, {
-  useCallback,
-  useContext,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
-import styles from "./chat.module.css";
+import { AuthentificationContext } from "@/context/AuthentificationContext/AuthentificationContext";
 import { ChatContext } from "@/context/ChatContext/ChatContext";
-import { Box, List, Paper, TextField, Typography } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ClearIcon from "@mui/icons-material/Clear";
-import SendIcon from "@mui/icons-material/Send";
-import { AuthentificationContext } from "@/context/AuthentificationContext/AuthentificationContext";
 import ForumIcon from "@mui/icons-material/Forum";
-import { useId } from "react";
+import SendIcon from "@mui/icons-material/Send";
+import { Box, Button, List, Paper, TextField, Typography } from "@mui/material";
+import { useCallback, useContext, useEffect, useId, useState } from "react";
+import styles from "./chat.module.css";
+import { useRef } from "react";
+import { toast } from "react-toastify";
 
 export const Chat = () => {
   const { messages, sendMessage } = useContext(ChatContext);
   const [text, setText] = useState<string>("");
+  const usernameInputRef = useRef<HTMLInputElement | null>(null);
   const { userDto } = useContext(AuthentificationContext);
+  const [username, setUsername] = useState<string | undefined>(
+    userDto?.username
+  );
   const [isChatVisible, setIsChatVisible] = useState<boolean>(false);
   const [isScrollDown, setIsScrollDown] = useState<boolean>(false);
+
+  if (username === undefined) {
+    if (
+      typeof window !== "undefined" &&
+      sessionStorage.getItem("username") !== null
+    ) {
+      setUsername(sessionStorage.getItem("username")!);
+    }
+  }
+
+  useEffect(() => {
+    if (userDto !== undefined) {
+      setUsername(userDto.username);
+    }
+  }, [userDto]);
 
   const messageListRef = useCallback(
     (node: HTMLDivElement) => {
@@ -59,7 +73,7 @@ export const Chat = () => {
     if (text.trim().localeCompare("") !== 0) {
       await sendMessage({
         text: text,
-        username: userDto?.username || "",
+        username: username!,
       });
       setText("");
     }
@@ -89,73 +103,116 @@ export const Chat = () => {
               }}
             />
           </div>
-          <Paper
-            ref={messageListRef}
-            key={listId}
-            style={{
-              backgroundColor: "white",
-              height: "100%",
-              overflow: "auto",
-            }}
-          >
-            <List sx={listStyle}>
-              {messages.map((message) => {
-                return (
-                  <Box
-                    sx={{
-                      display: "flex",
-                      borderTop: "1px solid white",
-                      paddingBlock: "4px",
-                    }}
-                  >
-                    <AccountCircleIcon />
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        paddingLeft: "4px",
-                      }}
-                    >
-                      <Typography sx={{ fontSize: "small" }}>
-                        {message.username}
-                      </Typography>
-                      <Typography>{message.text}</Typography>
-                    </Box>
-                  </Box>
-                );
-              })}
-            </List>
-          </Paper>
-          <div className={styles.sendMessageContainer}>
-            <TextField
-              multiline
-              value={text}
-              placeholder="Write your thoughts"
-              sx={{ width: "100%" }}
-              onKeyDown={async (event) => {
-                if (event.key === "Enter") {
-                  event.preventDefault();
-                  await handleSendMessage();
-                }
-              }}
-              onChange={(value) => {
-                setText(value.currentTarget.value);
-              }}
-              InputProps={{
-                endAdornment: (
-                  <SendIcon
-                    onClick={async () => {
+
+          {username !== undefined ? (
+            <>
+              <Paper
+                ref={messageListRef}
+                key={listId}
+                style={{
+                  backgroundColor: "white",
+                  height: "100%",
+                  overflow: "auto",
+                }}
+              >
+                <List sx={listStyle}>
+                  {messages.map((message) => {
+                    return (
+                      <Box
+                        sx={{
+                          display: "flex",
+                          borderTop: "1px solid white",
+                          paddingBlock: "4px",
+                        }}
+                      >
+                        <AccountCircleIcon />
+                        <Box
+                          sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            paddingLeft: "4px",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "small" }}>
+                            {message.username}
+                          </Typography>
+                          <Typography>{message.text}</Typography>
+                        </Box>
+                      </Box>
+                    );
+                  })}
+                </List>
+              </Paper>
+              <div className={styles.sendMessageContainer}>
+                <TextField
+                  multiline
+                  value={text}
+                  placeholder="Write your thoughts"
+                  sx={{ width: "100%" }}
+                  onKeyDown={async (event) => {
+                    if (event.key === "Enter") {
+                      event.preventDefault();
                       await handleSendMessage();
-                    }}
-                    sx={{
-                      color: `${text === "" ? "" : "blue"}`,
-                      cursor: `${text === "" ? "default" : "pointer"}`,
-                    }}
-                  />
-                ),
-              }}
-            ></TextField>
-          </div>
+                    }
+                  }}
+                  onChange={(value) => {
+                    setText(value.currentTarget.value);
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <SendIcon
+                        onClick={async () => {
+                          await handleSendMessage();
+                        }}
+                        sx={{
+                          color: `${text === "" ? "" : "blue"}`,
+                          cursor: `${text === "" ? "default" : "pointer"}`,
+                        }}
+                      />
+                    ),
+                  }}
+                ></TextField>
+              </div>
+            </>
+          ) : (
+            <>
+              <Box
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "10px",
+                }}
+              >
+                <Typography>Input your nickname</Typography>
+                <TextField
+                  sx={{ width: "85%" }}
+                  label="Username"
+                  inputRef={usernameInputRef}
+                ></TextField>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    const text = usernameInputRef.current?.value;
+                    if (text!.length < 3) {
+                      toast("Nickname should be at least 3 letters long", {
+                        type: "error",
+                      });
+                    } else {
+                      setUsername(text!);
+                      if (typeof window !== "undefined") {
+                        sessionStorage.setItem("username", text!);
+                      }
+                    }
+                  }}
+                >
+                  Connect
+                </Button>
+              </Box>
+            </>
+          )}
         </div>
       )}
     </div>
